121d36cd5af897c5a49a9e7572c622af98e3c78b
Lelchik <matsenko.olga@gmail.com>
2020-05-26 10:43:12 +0500

    Use redux state in mobile

--- index.native.js
+++ index.native.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
-
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './src/Mobile/App';
+import AppInternal from './src/Mobile/App';
 import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
+import reducer from './src/reducers/index';
+import { createStore } from 'redux';
 
+const App = () => {
+  return (
+    <Provider store={createStore(reducer)}>
+      <AppInternal />
+    </Provider>
+  );
+};
 AppRegistry.registerComponent(appName, () => App);
--- src/Mobile/App.tsx
+++ src/Mobile/App.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { connect, ConnectedProps } from 'react-redux';
+import {
+  ClearMatrix,
+  ChangeSelectedMatrix,
+  MultiplyMatrix,
+  ChangeBackground,
+  ChangePlace,
+  ChangeMatrixA,
+  ChangeMatrixB,
+  AddColumn,
+  AddRow,
+  DeleteColumn,
+  DeleteRow,
+} from '../actions/index';
+import { Matrix, MatrixName, Colors } from '../types';
 
-const App = () => {
-  return (
-    <>
-      <View>
-        <Text>123</Text>
-      </View>
-    </>
-  );
+export interface State {
+  matrixA: Matrix;
+  matrixB: Matrix;
+  matrixC: Matrix;
+  selectMatrix: MatrixName;
+  color: Colors;
+  error?: string;
+}
+const App = (props: Props) => {
+  return <Text>{JSON.stringify(props, null, 4)}</Text>;
 };
 
-export default App;
+function mapStateToProps(state: State) {
+  return {
+    ...state,
+  };
+}
+
+const mapDispatchToProps = {
+  ClearMatrix,
+  ChangeSelectedMatrix,
+  MultiplyMatrix,
+  ChangeBackground,
+  ChangePlace,
+  ChangeMatrixA,
+  ChangeMatrixB,
+  AddColumn,
+  AddRow,
+  DeleteColumn,
+  DeleteRow,
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+export type Props = ConnectedProps<typeof connector>;
+export default connector(App);
