ba43d9a27e4beeaffa270402d1ee85251a876fd3
Lelchik <matsenko.olga@gmail.com>
2020-05-28 10:26:17 +0500

    refactoring

--- src/Mobile/Matrix.tsx
+++ src/Mobile/Matrix.tsx
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 import {
   TouchableHighlight,
   Modal,
-  Text,
-  TextInput,
   View,
   StyleSheet,
   Button,
 } from 'react-native';
 import { Matrix as MatrixType } from '../types';
+import MatrixView from './MatrixView';
 
 interface Props {
   matrix: MatrixType;
@@ -20,21 +19,6 @@ interface Props {
 export const Matrix = (props: Props) => {
   const [editable, handleChangeEditable] = useState(false);
   const [modalMatrix, handleChangeMatrix] = useState(props.matrix);
-  function handleChangeValue(
-    rowIndex: number,
-    colIndex: number,
-    value: string,
-  ) {
-    if (!isNaN(Number(value))) {
-      const column = [...modalMatrix[rowIndex]];
-      column[colIndex] = Number(value);
-      handleChangeMatrix([
-        ...modalMatrix.slice(0, rowIndex),
-        column,
-        ...modalMatrix.slice(rowIndex + 1),
-      ]);
-    }
-  }
 
   function handleApplyChanges() {
     if (props.onChange != null) {
@@ -43,50 +27,13 @@ export const Matrix = (props: Props) => {
     handleChangeEditable(false);
   }
 
-  function renderColumn(
-    array: Array<number | undefined>,
-    rowIndex: number,
-    disabled?: boolean,
-  ) {
-    return array.map((col: number | undefined, colIndex: number) => (
-      <View
-        key={colIndex}
-        style={colIndex === array.length - 1 ? styles.lastCell : styles.cell}>
-        {disabled ? (
-          <TextInput
-            style={styles.input}
-            placeholder={`${props.name}${rowIndex},${colIndex}`}
-            keyboardType="number-pad"
-            value={col == null ? '' : col + ''}
-            onChangeText={(value) =>
-              handleChangeValue(rowIndex, colIndex, value)
-            }
-            editable={props.disabled}
-          />
-        ) : (
-          <Text style={styles.value}>{col == null ? '_' : col}</Text>
-        )}
-      </View>
-    ));
-  }
-
-  function renderMatrix(matrix?: MatrixType, disabled?: boolean) {
-    return (matrix || props.matrix).map(
-      (row: Array<number | undefined>, rowIndex: number) => (
-        <View style={styles.row} key={rowIndex}>
-          {renderColumn(row, rowIndex, disabled)}
-        </View>
-      ),
-    );
-  }
-
   return (
     <View style={styles.root}>
       <Modal animationType="fade" transparent={true} visible={editable}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            {renderMatrix(modalMatrix, true)}
-            <View style={{ flexDirection: 'row', alignItems: 'stretch' }}>
+            <MatrixView matrix={modalMatrix} onChange={handleChangeMatrix} />
+            <View style={styles.footer}>
               <Button title="Готово" onPress={handleApplyChanges}></Button>
               <Button
                 title="Отмена"
@@ -104,11 +51,7 @@ export const Matrix = (props: Props) => {
         onPress={() => {
           handleChangeEditable(true);
         }}>
-        <View
-          style={styles.container}
-          onTouchStart={() => handleChangeEditable(!editable)}>
-          {renderMatrix()}
-        </View>
+        <MatrixView matrix={props.matrix} disabled />
       </TouchableHighlight>
     </View>
   );
@@ -142,29 +85,6 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-  container: {
-    flex: 1,
-    padding: 5,
-  },
-  row: {
-    flexDirection: 'row',
-    marginBottom: 5,
-  },
-  cell: {
-    marginLeft: 5,
-  },
-  lastCell: {
-    marginRight: 5,
-    marginLeft: 5,
-  },
-  input: {
-    textAlign: 'center',
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-  value: {
-    textAlign: 'center',
-    textAlignVertical: 'center',
-  },
+  footer: { flexDirection: 'row', alignItems: 'stretch', marginTop: 5 },
 });
 export default Matrix;
---
+++ src/Mobile/MatrixView.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
+import { Matrix as MatrixType } from '../types';
+
+interface Props {
+  matrix: MatrixType;
+  disabled?: boolean;
+  onChange?: (matrix: MatrixType) => void;
+}
+
+export const MatrixView = (props: Props) => {
+  function handleChangeValue(
+    rowIndex: number,
+    colIndex: number,
+    value: string,
+  ) {
+    if (props.onChange != null && !isNaN(Number(value))) {
+      const column = [...props.matrix[rowIndex]];
+      column[colIndex] = Number(value);
+      props.onChange([
+        ...props.matrix.slice(0, rowIndex),
+        column,
+        ...props.matrix.slice(rowIndex + 1),
+      ]);
+    }
+  }
+
+  function renderColumn(array: Array<number | undefined>, rowIndex: number) {
+    return array.map((col: number | undefined, colIndex: number) => (
+      <View
+        key={colIndex}
+        style={colIndex === array.length - 1 ? styles.lastCell : styles.cell}>
+        {!props.disabled ? (
+          <TextInput
+            style={styles.input}
+            keyboardType="number-pad"
+            value={col == null ? '' : col + ''}
+            onChangeText={(value) =>
+              handleChangeValue(rowIndex, colIndex, value)
+            }
+          />
+        ) : (
+          <Text style={styles.value}>{col == null ? '_' : col}</Text>
+        )}
+      </View>
+    ));
+  }
+
+  function renderMatrix() {
+    return props.matrix.map(
+      (row: Array<number | undefined>, rowIndex: number) => (
+        <View style={styles.row} key={rowIndex}>
+          {renderColumn(row, rowIndex)}
+        </View>
+      ),
+    );
+  }
+
+  return (
+    <View
+      style={{ display: 'flex', alignItems: 'stretch', flexDirection: 'row' }}>
+      <View style={{ ...styles.border, ...styles.leftBorder }} />
+      <View style={styles.matrix}>{renderMatrix()}</View>
+      <View style={{ ...styles.border, ...styles.rightBorder }} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  matrix: {
+    padding: 3,
+  },
+  border: {
+    width: 3,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 3,
+  },
+  rightBorder: {
+    borderLeftColor: 'white',
+    borderLeftWidth: 0,
+  },
+  leftBorder: {
+    borderRightColor: 'white',
+    borderRightWidth: 0,
+  },
+
+  row: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  cell: {
+    marginLeft: 5,
+  },
+  lastCell: {
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  input: {
+    textAlign: 'center',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  value: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+});
+export default MatrixView;
