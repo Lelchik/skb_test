8c494de1efa7c40323d0494c566459b14a168e53
Lelchik <matsenko.olga@gmail.com>
2020-05-29 11:08:54 +0500

    Show all matrix in mobile

--- src/Mobile/MatrixPanel.tsx
+++ src/Mobile/MatrixPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Matrix from './Matrix';
 import { Colors, Matrix as MatrixType } from '../types';
+import { View, StyleSheet, Text } from 'react-native';
 
 interface Props {
   matrixC: MatrixType;
@@ -13,12 +14,56 @@ interface Props {
 
 export const MatrixPanel = (props: Props) => {
   return (
-    <Matrix
-      matrix={props.matrixA}
-      disabled
-      name="a"
-      onChange={props.onChangeMatrixA}></Matrix>
+    <View style={styles.root}>
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <Matrix matrix={props.matrixC} disabled />
+        </View>
+        <View style={styles.item}>
+          <Matrix matrix={props.matrixA} onChange={props.onChangeMatrixA} />
+        </View>
+        <View style={styles.item}>
+          <Text style={styles.name}>A</Text>
+        </View>
+      </View>
+      <View style={styles.matrixB}>
+        <View style={styles.item}>
+          <Matrix matrix={props.matrixB} onChange={props.onChangeMatrixB} />
+        </View>
+        <View style={styles.item}>
+          <Text
+            style={{
+              ...styles.name,
+              paddingLeft: props.matrixB.length * 10,
+            }}>
+            B
+          </Text>
+        </View>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  item: {
+    margin: 5,
+  },
+  matrixB: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  name: {
+    fontSize: 20,
+  },
+});
 
 export default MatrixPanel;
